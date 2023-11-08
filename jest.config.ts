c28fa756cb743983d5216a2e67bca1e04393a881
Анна Красновид <88835714+AnnaKrasnovid@users.noreply.github.com>
2023-11-08 20:20:28 +0300

    config

--- jest.config.ts
+++ jest.config.ts
@@ -1,25 +1,3 @@
-// const nextJest = require('next/jest')
-
-// const createJestConfig = nextJest({ 
-//   dir: './'
-// })
-
-// const customConfig = { 
-//   'clearMocks': true,
-//   'coverageDirectory': '.coverage',
-//   'moduleDirectories': ['node_modules', '<rootDir>/'],
-//   'setupFilesAfterEnv': ['<rootDir>/jest.setup.js'],
-//   'testEnvironment': 'jest-environment-jsdom',
-//   'preset': 'ts-jest',
-//   // 'transform': {
-//   //   '^.+\\.(ts|tsx|js)$': 'babel-jest', // this is probably something you already had, if using ts-jest, it's probably fine to leave as ts-jest
-//   //   '^.+\\.(css)$': '<rootDir>/config/jest/fileTransform.js', // add this to fix css import issues
-//   // },
-//   // 'transformIgnorePatterns': ["node_modules/(?!(swiper|ssr-window|dom7)/)"],
-// }
-
-// module.exports = createJestConfig(customConfig)
-
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
